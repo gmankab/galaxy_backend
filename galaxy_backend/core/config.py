798416f985553d30d01f4bf105b324b2422b4409
imneokitty <imneokitty@gmail.com>
2024-06-07 19:09:15 +0300

    [+] channel subscription requirement

--- galaxy_backend/core/config.py
+++ galaxy_backend/core/config.py
@@ -1,21 +1,17 @@
 import os
 
-
 class env:
     game_url: str = 'https://gmanka.gitlab.io/galaxy_frontend'
     db_url: str = 'sqlite://:memory:'
     tg_token: str = ''
     tests: str = ''
-
+    channel_username: str = '@foo'  # replace foo with an actual channel username
 
 def set_env():
     for key, value_type in env.__annotations__.items():
         value = os.getenv(key)
         if value:
-            assert isinstance(value_type, type)
             assert isinstance(value, value_type)
             setattr(env, key, value)
 
-
 set_env()
-
--- galaxy_backend/tg/game.py
+++ galaxy_backend/tg/game.py
@@ -1,10 +1,11 @@
-from core.common import all
+from core.common import all, tg
 import core.config
 import models.db
 import aiogram.types
 import aiogram.utils
 import aiogram
 
+require_channel_subscription: str = core.config.env.channel_username
 
 class inline_keyboard:
     button = aiogram.types.InlineKeyboardButton(
@@ -17,13 +18,36 @@ class inline_keyboard:
         inline_keyboard=[[button]]
     )
 
-
 @all.dp.message()
 async def on_message(
     msg: aiogram.types.Message,
 ) -> None:
     if not msg.from_user:
         return
+
+    try:
+        user_status = await msg.bot.get_chat_member(
+            chat_id=require_channel_subscription,
+            user_id=msg.from_user.id
+        )
+
+        if user_status.status not in ['member', 'administrator', 'creator']:
+            await msg.answer(
+                text='Please subscribe to our channel to start the game.',
+                reply_markup=aiogram.types.InlineKeyboardMarkup(
+                    inline_keyboard=[[
+                        aiogram.types.InlineKeyboardButton(
+                            text='Subscribe',
+                            url=f'https://t.me/{require_channel_subscription.lstrip("@")}'
+                        )
+                    ]]
+                )
+            )
+            return
+    except aiogram.exceptions.TelegramBadRequest as e:
+        await msg.answer(f"Error: {str(e)}")
+        return
+
     await models.db.User.get_or_create(
         tg_id=msg.from_user.id,
         coins=0,
@@ -32,4 +56,3 @@ async def on_message(
         text='hello',
         reply_markup=inline_keyboard.markup,
     )
-
