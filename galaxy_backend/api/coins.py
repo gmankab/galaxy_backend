fcf6397ee35f32efb67155ef2edb729edcc18da0
Grisha <grisha765@tuta.io>
2024-06-07 19:56:35 +0300

    [u] add autoclicker

--- galaxy_backend/api/coins.py
+++ galaxy_backend/api/coins.py
@@ -4,7 +4,7 @@ import models.other
 import models.coins
 import models.db
 import fastapi
-
+import asyncio
 
 @api.routers.coin.post('/add')
 async def set_coins(
@@ -45,3 +45,31 @@ async def get_coins(
         coins=user.coins,
     )
 
+async def autoclick_task(tg_id: int, interval: int, duration: int):
+    '''
+    Фоновая задача для добавления монет пользователю с заданным интервалом и длительностью.
+    '''
+    start_time = asyncio.get_event_loop().time()
+    end_time = start_time + duration
+    while asyncio.get_event_loop().time() < end_time:
+        try:
+            user = await models.db.User.get(tg_id=tg_id)
+            user.coins += 1  # Добавляем одну монету за каждый интервал
+            await user.save()
+        except tortoise.exceptions.DoesNotExist:
+            # Если пользователь не найден, прерываем выполнение
+            break
+        await asyncio.sleep(interval)
+
+@api.routers.coin.post('/autoclick')
+async def start_autoclick(
+    tg_id: int,
+    interval: int,
+    duration: int,
+    background_tasks: fastapi.BackgroundTasks
+) -> models.other.Success:
+    '''
+    Запускает автокликер для добавления монет периодически.
+    '''
+    background_tasks.add_task(autoclick_task, tg_id, interval, duration)
+    return models.other.Success()
