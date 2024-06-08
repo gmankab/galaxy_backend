9f977d3391b90af7b7c12ec56b88614f1701fd73
Grisha <grisha765@tuta.io>
2024-06-08 16:38:43 +0300

    [u] update run tests

--- galaxy_backend/tests/run.py
+++ galaxy_backend/tests/run.py
@@ -1,14 +1,16 @@
-from tests import init, coins
+from tests import init, coins, tg
 from core.common import all
 import core.shutdown
-
 
 async def main():
     to_run = [
         init.create_user,
         coins.get_coins,
         coins.add_coins,
-        coins.start_autoclick
+        coins.start_autoclick,
+        tg.test_on_message_user_not_subscribed,
+        tg.test_on_message_user_subscribed,
+        tg.test_on_message_no_user
     ]
     for func in to_run:
         msg = await func()
