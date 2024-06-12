78836fd89ca39da50f9f8f35bd0bbe9991a39e07
gmankab <gmankab@gmail.com>
2024-06-12 11:28:12 +0400

    [f] pyright warn

--- galaxy_backend/api/clans.py
+++ galaxy_backend/api/clans.py
@@ -1,6 +1,7 @@
 from models.db import User, Clan, ClanOwner
 
-async def create_clan(clan_name: str, clan_owner_id: int) -> Clan | None:
+
+async def create_clan(clan_name: str, clan_owner_id: int) -> Clan:
     clan_owner_user = await User.get(tg_id = clan_owner_id)
     clan = await Clan.create(name = clan_name)
     clan_owner = await ClanOwner.create(user=clan_owner_user, clan=clan)
@@ -8,24 +9,28 @@ async def create_clan(clan_name: str, clan_owner_id: int) -> Clan | None:
     await clan_owner.save()
     return clan
 
-async def add_user(clan_id: int, user_id: int):
+
+async def add_user(clan_id: int, user_id: int) -> None:
     user = await User.get(tg_id = user_id)
     clan = await Clan.get(id = clan_id)
     user.clan = clan
     await user.save()
     return
 
-async def remove_user(user_id: int):
+
+async def remove_user(user_id: int) -> None:
     user = await User.get(tg_id = user_id)
     user.clan = None
     await user.save()
     return
 
-async def get_owner(clan_id: int):
+
+async def get_owner(clan_id: int) -> int:
     req_clan = await Clan.get(id = clan_id)
     clan_owner = await ClanOwner.get(clan = req_clan)
     return clan_owner.user.tg_id
 
-async def get_members(clan_id: int):
-    clan = await Clan.get(id = clan_id).prefetch_related("members")
-    return clan.members
+
+async def get_members(clan_id: int) -> list[User]:
+    return await User.filter(clan_id=clan_id)
+
