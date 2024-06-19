155895aabb66494cd98763d24bfb25918a52105c
Grisha <grisha765@tuta.io>
2024-06-19 17:01:56 +0300

    [u] init router

--- galaxy_backend/core/main.py
+++ galaxy_backend/core/main.py
@@ -3,8 +3,10 @@ import tg.game as __game__
 import api.autoclick
 import api.app
 import api.coins
+import api.clans
 import api.routers
 
 app = api.app.app
 app.include_router(api.routers.coin)
+app.include_router(api.routers.clan)
 
