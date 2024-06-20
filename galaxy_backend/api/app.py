6e270e72e733b58c83989d1742be25d32e697350
gmankab <gmankab@gmail.com>
2024-06-20 19:33:53 +0400

    [+] planets and sectors creating

--- galaxy_backend/api/app.py
+++ galaxy_backend/api/app.py
@@ -13,19 +13,38 @@ import asyncio
 import httpx
 
 
-async def init_db():
-    await tortoise.Tortoise.init(
-        db_url=core.config.env.db_url,
-        modules={'models': ['models.db']}
-    )
-    await tortoise.Tortoise.generate_schemas()
-
-
-@contextlib.asynccontextmanager
-async def lifespan(
-    __fastapi_app__,
-):
+async def init_all():
     await init_db()
+    await start_bot()
+    await create_planets_and_sectors()
+    asyncio.create_task(api.autoclick.background_clicker())
+
+
+async def create_planets_and_sectors():
+    for planet_id in range(core.config.planet.count):
+        sector_id = planet_id // core.config.planet.per_sector
+        if sector_id == 0:
+            available = True
+        else:
+            available = False
+        sector, created = await models.db.Sector.get_or_create(
+            id=sector_id,
+            defaults={
+                'available': available,
+            }
+        )
+        if created:
+            await sector.save()
+        if not models.db.Planet.filter(id=planet_id).exists():
+            planet_resources = planet_id * core.config.planet.resources_step + core.config.planet.initial_resources_count
+            await models.db.Planet.create(
+                id=planet_id,
+                sector=sector,
+                resources=planet_resources,
+            )
+
+
+async def start_bot():
     if core.config.env.tg_token:
         asyncio.create_task(
             all.dp.start_polling(tg.bot)
@@ -38,7 +57,20 @@ async def lifespan(
             coins=0,
             autoclicks_remain=0,
         )
-    asyncio.create_task(api.autoclick.background_clicker())
+
+async def init_db():
+    await tortoise.Tortoise.init(
+        db_url=core.config.env.db_url,
+        modules={'models': ['models.db']}
+    )
+    await tortoise.Tortoise.generate_schemas()
+
+
+@contextlib.asynccontextmanager
+async def lifespan(
+    __fastapi_app__,
+):
+    await init_all()
     yield
     await core.shutdown.shutdown()
 
