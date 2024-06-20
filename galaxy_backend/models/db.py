65ffd1c984359f7ba46b74deeb0b1b4ca251a258
gmankab <gmankab@gmail.com>
2024-06-20 04:43:13 +0400

    [f] formatting

--- galaxy_backend/models/db.py
+++ galaxy_backend/models/db.py
@@ -6,12 +6,15 @@ class User(Model):
     tg_id = fields.IntField(primary_key=True)
     coins = fields.IntField(default=0)
     autoclicks_remain = fields.IntField(default=0)
-    clan = fields.ForeignKeyField("models.Clan", related_name="members", null=True)
+    clan = fields.ForeignKeyField('models.Clan', related_name='members', null=True)
+
 
 class Clan(Model):
     id = fields.IntField(primary_key=True)
     name = fields.CharField(max_length=30)
 
+
 class ClanOwner(Model):
-    clan = fields.OneToOneField("models.Clan", related_name="owner_clan")
-    user = fields.OneToOneField("models.User", related_name="clan_owner")
+    clan = fields.OneToOneField('models.Clan', related_name='owner_clan')
+    user = fields.OneToOneField('models.User', related_name='clan_owner')
+
